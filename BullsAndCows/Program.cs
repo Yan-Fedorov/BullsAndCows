d6b997f71dbb12cba2ceb8915181994903270303
valeriy_tereshenko <tereshenko.valeriy.v@gmail.com>
2017-09-13 22:21:54 +0300

    решатель

--- BullsAndCows/Program.cs
+++ BullsAndCows/Program.cs
@@ -9,49 +9,9 @@ namespace BullsAndCows
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            var number = new Random().Next(100, 999);
-
-            Console.WriteLine("Я загадал 3-х значное число, угадывай )");
-
-            var iteration = 0;
-            do
-            {
-                Console.WriteLine();
-                var assumption = GetNumber();
-
-                if (assumption > number)
-                {
-                    Console.WriteLine("Загаданное число меньше");
-                }
-                else if (assumption < number)
-                {
-                    Console.WriteLine("Загаданное число больше");
-                }
-                else
-                    break;
-
-                iteration++;
-            } while (iteration < 10);
-
-            Console.WriteLine();
-            Console.WriteLine(iteration == 10
-                ? $"К сожалению у вас закончились попытки. Я загадывал {number}"
-                : $"Верно, я загадывал {number}"
-            );
+            new Solwer().Run();
 
             Console.ReadLine();
-        }
-
-        private static int GetNumber()
-        {
-            while (true)
-            {
-                Console.Write("Введите число: ");
-
-                var st = Console.ReadLine();
-                if (int.TryParse(st, out var num))
-                    return num;
-            }
         }
     }
 }
---
+++ BullsAndCows/Solwer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace BullsAndCows
+{
+    public enum Estimation
+    {
+        Equal = 1, Less, More
+    }
+
+    public class Solwer
+    {
+        public void Run()
+        {
+            Console.WriteLine("Загадайте 3-х значное число");
+            Console.WriteLine();
+
+
+            var line = 500;
+            var assumption = line;
+            var iteration = 10;
+            var guessed = false;
+
+            do
+            {
+                iteration--;
+
+                line = line / 2;
+                if (line == 0)
+                    line = 1;
+
+                Console.WriteLine($"Вы загадали {assumption} ?");
+
+                var estimate = GetEstimation();
+                switch (estimate)
+                {
+                    case Estimation.Equal:
+                        guessed = true;
+                        break;
+
+                    case Estimation.Less:
+                        assumption -= line;
+                        break;
+
+                    case Estimation.More:
+                        assumption += line;
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+            } while (iteration > 0 && !guessed);
+
+            Console.WriteLine(guessed
+                ? "Ура, я угадал )))"
+                : "Ой какое сложное число вы загадали");
+
+            Console.ReadLine();
+
+        }
+
+        private static Estimation GetEstimation()
+        {
+            Console.WriteLine(@"
+ 1 - да
+ 2 - моё число меньше
+ 3 - моё число больше");
+
+            while (true)
+            {
+                Console.Write("Укажите соответствующий вариант: ");
+
+                var key = Console.ReadLine();
+
+                if (int.TryParse(key, out var num) && num > 0 && num <= 3)
+                    return (Estimation)num;
+            }
+        }
+    }
+}
