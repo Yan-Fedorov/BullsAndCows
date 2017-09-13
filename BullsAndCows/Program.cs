eeecab3d550af2b26e8c39c38fe5b6ba36c4e518
valeriy_tereshenko <tereshenko.valeriy.v@gmail.com>
2017-09-13 21:48:20 +0300

    ощибка больше потом меньше в угадайке

--- BullsAndCows/Program.cs
+++ BullsAndCows/Program.cs
@@ -10,26 +10,30 @@ namespace BullsAndCows
             Console.OutputEncoding = Encoding.UTF8;
 
             var number = new Random().Next(100, 999);
-            Console.WriteLine(number);
 
             Console.WriteLine("Я загадал 3-х значное число, угадывай )");
 
             var iteration = 0;
             do
             {
+                Console.WriteLine();
                 var assumption = GetNumber();
 
                 if (assumption > number)
+                {
                     Console.WriteLine("Загаданное число меньше");
-
-                if (assumption < number)
+                }
+                else if (assumption < number)
+                {
                     Console.WriteLine("Загаданное число больше");
+                }
                 else
                     break;
 
                 iteration++;
             } while (iteration < 10);
 
+            Console.WriteLine();
             Console.WriteLine(iteration == 10
                 ? $"К сожалению у вас закончились попытки. Я загадывал {number}"
                 : $"Верно, я загадывал {number}"
