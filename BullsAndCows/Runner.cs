74b2839346574f85e8663bfb5d20bdf38dd4d9c5
valeriy_tereshenko <tereshenko.valeriy.v@gmail.com>
2017-09-16 21:34:14 +0300

    начало - с отдельным Runnerom - v.1.0.1

---
+++ BullsAndCows/Runner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsAndCows
+{
+    public enum Game
+    {
+        Solver = 1, Puzzle
+    }
+
+    public class Runner
+    {
+        public void Run()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
+            {
+                Console.Clear();
+                var game = SelectGame();
+
+                switch (game)
+                {
+                    case Game.Solver:
+                        new Solwer().Run();
+                        break;
+
+                    case Game.Puzzle:
+                        new Puzzle().Run();
+                        break;
+
+                    case null:
+                        Console.WriteLine("Как жаль что вы уже уходите.");
+                        Console.ReadLine();
+                        return;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private static Game? SelectGame()
+        {
+            Console.WriteLine(@"Укажите вариант игры:
+1 - компьютер отгадывает число
+2 - компьютер загадывает число
+3 - выход");
+
+            while (true)
+            {
+                var key = Console.ReadLine();
+
+                if (int.TryParse(key, out var num) && num > 0 && num < 4)
+                    return num == 3 ? (Game?)null : (Game)num;
+            }
+        }
+    }
+}
