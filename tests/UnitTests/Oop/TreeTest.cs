3b8e0989cd148ce349a4b9f6a7e2edb572ba67dc
Yan-Fedorov <axxxx@ukr.net>
2017-11-17 21:30:07 +0200

    Algorithm 2 for Solwer

---
+++ tests/UnitTests/Oop/TreeTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Oop
+{
+    public class TreeTest
+    {
+        private class TreeNode
+        {
+            public int? Id { get; set; }
+            public TreeNode L { get; set; }
+            public TreeNode R { get; set; }
+        }
+
+        private TreeNode BuildTree(int deep)
+        {
+            var node = new TreeNode();
+
+            if (deep > 0)
+            {
+                node.L = BuildTree(deep - 1);
+                node.R = BuildTree(deep - 1);
+            }
+
+            return node;
+        }
+
+        int x;
+
+        private void Enumerate(TreeNode node)
+        {
+            if (node == null) return;
+            x++;
+            Enumerate(node.L);
+            Enumerate(node.R);
+        }
+
+        [Fact]
+        public void Run()
+        {
+            x = 0;
+            Enumerate(BuildTree(0));
+            Assert.Equal(1, x);
+
+            x = 0;
+            Enumerate(BuildTree(1));
+            Assert.Equal(3, x);
+
+            x = 0;
+            Enumerate(BuildTree(2));
+            Assert.Equal(7, x);
+
+
+            x = 0;
+            Enumerate(BuildTree(8));
+
+        }
+    }
+}
